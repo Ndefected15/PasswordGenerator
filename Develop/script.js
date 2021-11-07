8c64d716ca1d52bf073c4da548243ca38ab64cc3
Nick D <Ndefected15@gmail.com>
2021-11-07 13:41:25 -0500

    due nov 14

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,21 @@
 // Assignment code here
+function generatePassword() {
+  var lengthPrompt = window.prompt(
+    "Choose Password length between 8 and 128 characters:"
+  );
 
+  if (
+    lengthPrompt === "" ||
+    lengthPrompt === null ||
+    lengthPrompt > 128 ||
+    lengthPrompt < 8
+  ) {
+    window.alert("You need to enter a number between 8 and 128. Try Again.");
+    return generatePassword();
+  } else {
+    return lengthPrompt;
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +26,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
